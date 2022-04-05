d5daab91e0b16a4af18e2b3106cf295e9bca231f
dufresnesteven <steve.dufresne@automattic.com>
2022-04-05 15:44:34 +0900

    Integrate new header.

---
+++ env/0-sandbox.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+ * These are stubs for closed source code, or things that only apply to local environments.
+ */
+
+namespace WordPress_org\WporgGutenberg\Stubs;
+
+defined( 'WPINC' ) || die();
+
+require_once WPMU_PLUGIN_DIR . '/wporg-mu-plugins/mu-plugins/loader.php';
