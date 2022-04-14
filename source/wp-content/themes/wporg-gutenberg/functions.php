078c6b807c109aa2e9e1912269710bab1c3ded1d
dufresnesteven <steve.dufresne@automattic.com>
2022-04-14 15:02:19 +0900

    Remove the extra template stuff.

--- source/wp-content/themes/wporg-gutenberg/functions.php
+++ source/wp-content/themes/wporg-gutenberg/functions.php
@@ -790,30 +790,3 @@ function gutenbergtheme_scripts() {
 	wp_enqueue_style( 'gutenbergtheme-style', get_stylesheet_uri(), array(), 14 );
 }
 add_action( 'wp_enqueue_scripts', 'gutenbergtheme_scripts' );
-
-/**
- * Implement the Custom Header feature.
- */
-require get_template_directory() . '/inc/custom-header.php';
-
-/**
- * Custom template tags for this theme.
- */
-require get_template_directory() . '/inc/template-tags.php';
-
-/**
- * Functions which enhance the theme by hooking into WordPress.
- */
-require get_template_directory() . '/inc/template-functions.php';
-
-/**
- * Customizer additions.
- */
-require get_template_directory() . '/inc/customizer.php';
-
-/**
- * Load Jetpack compatibility file.
- */
-if ( defined( 'JETPACK__VERSION' ) ) {
-	require get_template_directory() . '/inc/jetpack.php';
-}
