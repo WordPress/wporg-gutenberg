a25bd42981fde596106b5825fc71318bf415a39e
dufresnesteven <steve.dufresne@automattic.com>
2022-04-25 14:38:38 +0900

    Create a link component.

---
+++ source/wp-content/themes/wporg-gutenberg/blocks/link/link.js
@@ -0,0 +1,30 @@
+var el = wp.element.createElement,
+	registerBlockType = wp.blocks.registerBlockType,
+	InnerBlocks = wp.blockEditor.InnerBlocks;
+
+registerBlockType( 'wporg/wporg-gutenberg-link', {
+	title: 'Demo Link',
+	icon: 'button',
+	category: 'layout',
+
+	edit: function ( props ) {
+		if ( ! window.location.pathname.includes( 'wp-admin' ) ) {
+			var blockEditorData = wp.data.select( 'core/block-editor' );
+			var innerHtml = blockEditorData.getBlock( props.clientId ).innerBlocks[ 0 ].originalContent;
+
+			return el( 'div', { className: props.className, dangerouslySetInnerHTML: { __html: innerHtml } } );
+		}
+		return el(
+            'div',
+            { className: props.className },
+            el( InnerBlocks, {
+    			template: [ [ 'core/paragraph' ] ],
+    			templateLock: 'all',
+		    } 
+        ) );
+	},
+
+	save: function ( props ) {
+		return el( 'div', {}, el( InnerBlocks.Content ) );
+	},
+} );
